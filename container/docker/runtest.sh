069330ae4061d52fe7f4449bc277ce396dc16cef
Major Hayden <major@redhat.com>
2019-01-28 09:21:22 +0100

    Install ansible via packages

--- container/docker/runtest.sh
+++ container/docker/runtest.sh
@@ -3,31 +3,37 @@
 # Source the common test script helpers
 . /usr/bin/rhts_environment.sh
 
-# Try to install python
+# Try to install ansible
 if [ -x "/usr/bin/dnf" ]; then
-    dnf -y install python36
+    dnf -y install ansible
 else
-    yum -y install python
-    if [ $? -ne 0 ]; then
-        yum -y install python36
-    fi
+    yum -y install ansible
 fi
 
+# Try to install python
+# if [ -x "/usr/bin/dnf" ]; then
+#     dnf -y install python36
+# else
+#     yum -y install python
+#     if [ $? -ne 0 ]; then
+#         yum -y install python36
+#     fi
+# fi
 
 # Install pre-requisites for Ansible
-wget -O /opt/get-pip.py https://bootstrap.pypa.io/get-pip.py
-python /opt/get-pip.py
-pip install virtualenv
-virtualenv /opt/ansible-venv
+# wget -O /opt/get-pip.py https://bootstrap.pypa.io/get-pip.py
+# python /opt/get-pip.py
+# pip install virtualenv
+# virtualenv /opt/ansible-venv
 
 # Install the latest stable ansible version
-/opt/ansible-venv/bin/pip install ansible
+# /opt/ansible-venv/bin/pip install ansible
 
 # Ensure ansible is installed
-/opt/ansible-venv/bin/ansible --version
+ansible --version
 
 # Run Ansible
-/opt/ansible-venv/bin/ansible-playbook -i hosts.txt playbook.yml
+ansible-playbook -i hosts.txt playbook.yml
 
 if [ $? -eq 0 ]; then
     report_result finished PASS 0
