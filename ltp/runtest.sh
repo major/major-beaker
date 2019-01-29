dccfa4e3a985908ccb4bb3f2bc1a71b5eeb904b2
Major Hayden <major@redhat.com>
2019-01-29 13:10:25 +0100

    Update playbook

--- ltp/runtest.sh
+++ ltp/runtest.sh
@@ -3,30 +3,6 @@
 # Source the common test script helpers
 . /usr/bin/rhts_environment.sh
 
-# Try to install python
-if [ "${FAMILY}" == "RedHatEnterpriseLinux8" ]; then
-    dnf -y install python36 python36-devel
-elif [ "${FAMILY}" == "RedHatEnterpriseLinux7" ]; then
-    yum -y install python python-devel
-elif [ "${FAMILY}" == "Fedora29" ]; then
-    dnf -y install python36 python36-devel
-fi
-
-# Make python symlink if needed.
-if [ ! -x "/usr/bin/python" ]; then
-    ln -sf /usr/bin/python3.6 /usr/bin/python
-    python --version
-fi
-
-# Install pre-requisites for Ansible
-wget -O /opt/get-pip.py https://bootstrap.pypa.io/get-pip.py
-python /opt/get-pip.py
-pip install virtualenv
-virtualenv /opt/ansible-venv
-
-# Install the latest stable ansible version
-/opt/ansible-venv/bin/pip install ansible
-
 # Ensure ansible is installed
 /opt/ansible-venv/bin/ansible --version
 
